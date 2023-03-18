1e02d0dcc40dfbe65102c47c5e65723284d22668
Fang Liu <lfxjtu@gmail.com>
2023-03-18 17:24:10 +1300

    stash for now

---
+++ src/main/java/io/lfxjtu/springsecurityjwt/HelloResource.java
@@ -0,0 +1,12 @@
+package io.lfxjtu.springsecurityjwt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloResource {
+    @RequestMapping({"/hello"})
+    public String hello() {
+        return "Hello World";
+    }
+}
